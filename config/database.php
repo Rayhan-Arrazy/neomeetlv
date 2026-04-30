ef90e0d62006c627981bfd3b031090256bccc910
rayhanarrazy <rayhanarrazy08@gmail.com>
2026-04-30 22:52:41 +0700

    connection solve

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => 'mysql',
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -47,7 +47,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
 
-            'database' => 'neomeetdb', 
+            'database' => env('DB_DATABASE', 'laravel'), 
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
